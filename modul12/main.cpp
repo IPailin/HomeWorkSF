c497b6adb65d8dcf38a7cb17f320a8b5324616f0
IPaulin <iapaulin.sib@gmail.com>
2022-12-25 18:25:09 +0700

    Доработан алгоритм, удалена временная переменная, добавлены проверки данных

--- modul12/main.cpp
+++ modul12/main.cpp
@@ -4,40 +4,41 @@
 using namespace std;
 
 
-bool isKPeriodic(const string &text, int k){
+bool isKPeriodic(const string& text, const int k) {
 
-    
-    // int n = str.size();
-    // char arr[n] = {0};
+    //Проводим проверки входных данных
+    if (text.empty()) {
+        return false;
+    }
 
-    string str;
-    str.append(1,text[0]);
+    if (k < 1) {
+        return false;
+    }
 
-    int lenght {1};
+    int lenght{ 1 };
+    //вычисляем длинну уникальной строки из которой состоит text
+    for (size_t i = 1; i < text.size(); ++i) {
 
-    //char start_simbol = text[0];
-
-    for(size_t i = 1; i < text.size(); ++i){
-
-        if(text[i] != str[0]){
-            str.append(1,text[i]);
+        if (text[i] != text[0]) {           
             ++lenght;
         }
-        else{
+        else {
             break;
         }
     }
 
-    if((text.size() / lenght) != k){
+    //выполняем быструю  проверку на кратность
+    if ((text.size() / lenght) != k) {
         return false;
     }
 
-    for(size_t i = lenght; i < text.size(); i+=lenght){
-        for(size_t j = 0; j < lenght; ++j){
-            if(str[j] == text[j + i]){
+    //проводим проверку что остальной text состоит из уникальной строки полученной ранее
+    for (size_t i = lenght; i < text.size(); i += lenght) {
+        for (size_t j = 0; j < lenght; ++j) {
+            if (text[j] == text[j + i]) {
                 continue;
             }
-            else{
+            else {
                 return false;
             }
         }
@@ -45,10 +46,30 @@ bool isKPeriodic(const string &text, int k){
 
     return true;
 }
- 
+
 int main()
 {
-    isKPeriodic("adcadcadc", 3);
+    //немного примеров для тестирования
+    bool result = isKPeriodic("adcadcadc", 3);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("adcadcadcd", 3);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("adcadcadd", 3);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("aadcadcadd", 2);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("adcadc", 2);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("", 1);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
+
+    result = isKPeriodic("adc", 1);
+    result != 0 ? cout << "true" << endl : cout << "false" << endl;
 
     return 0;
 }
